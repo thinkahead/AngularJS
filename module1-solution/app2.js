d5a9fb90cd273441f38eddec1171e876e18e62b9
Alexei Karve <karve@us.ibm.com>
2016-09-22 21:05:58 -0400

    Additional Module1 Solution

---
+++ module1-solution/app2.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.numberMenuItems = 0;
+  $scope.checkList = function () {
+    if(typeof($scope.lunchMenuList) == 'undefined' || $scope.lunchMenuList.trim() == "") {
+      $scope.message = "Please enter data first";
+      $scope.msgColor = "red";
+      $scope.fieldColor = "has-error"
+    } else {
+      $scope.msgColor = "green";
+      $scope.fieldColor = "has-success"
+      if (countMenuItems() <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    }
+  };
+
+  function countMenuItems() {
+    return $scope.lunchMenuList.trim().split(",").length;
+  }
+}
+
+})();
+
